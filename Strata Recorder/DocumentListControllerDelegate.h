b5c2ba5ae26197dd1f7712cd247156a2216c5884
daltman <daltman@Don-Altmans-MacBook-Pro.local>
2012-11-21 21:54:32 -0800

    First version

---
+++ Strata Recorder/DocumentListControllerDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DocumentListControllerDelegate.h
+//  Strata Recorder
+//
+//  Created by daltman on 11/21/12.
+//  Copyright (c) 2012 Don Altman. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol DocumentListControllerDelegate <NSObject>
+
+- (void)handleExportPDFButton:(id)sender;
+
+@end
