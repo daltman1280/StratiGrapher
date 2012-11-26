b5de3df86737c1dc1e8326702ed6589bcc14ad11
daltman <daltman@Don-Altmans-MacBook-Pro.local>
2012-11-25 22:23:36 -0800

    Added

---
+++ SettingsTableController.h
@@ -0,0 +1,17 @@
+//
+//  SettingsTableController.h
+//  Strata Recorder
+//
+//  Created by daltman on 11/25/12.
+//  Copyright (c) 2012 Don Altman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsTableController : UITableViewController {
+	
+}
+
+@property id delegate;
+
+@end
