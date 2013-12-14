50315d8fdcf8fc9f0912f990cd67c3d4aab36b79
daltman <daltman@Don-Altmans-MacBook-Pro.local>
2013-12-13 19:20:53 -0800

    Fix, in case old versions of document do not have correct grainSizeIndex for each stratum.

--- StrataPageView.m
+++ StrataPageView.m
@@ -78,6 +78,9 @@
 
 - (void)drawColumnAdornments:(int)columnNumber columnOrigin:(CGPoint)columnOrigin minGrainSizeIndex:(int)minGrainSizeIndex maxGrainSizeIndex:(int)maxGrainSizeIndex
 {
+	// fix, in case old versions of document do not have correct grainSizeIndex for each stratum
+	if (minGrainSizeIndex <= 0) minGrainSizeIndex = 1;
+	if (maxGrainSizeIndex < minGrainSizeIndex) maxGrainSizeIndex = minGrainSizeIndex;
 	{	// column number
 		CGContextRef tempContext = UIGraphicsGetCurrentContext();
 		CGContextSaveGState(tempContext);
