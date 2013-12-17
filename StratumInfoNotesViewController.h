5d1d60e5c1363933ac0c89023eeb30271fc81f29
daltman <daltman@Don-Altmans-MacBook-Pro.local>
2013-12-16 17:23:45 -0800

    Files to be deleted from git.

---
+++ StratumInfoNotesViewController.h
@@ -0,0 +1,13 @@
+//
+//  StratumInfoNotesViewController.h
+//  Strata Recorder
+//
+//  Created by daltman on 12/15/13.
+//  Copyright (c) 2013 Don Altman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StratumInfoNotesViewController : UIView
+
+@end
---
+++ StratumInfoNotesViewController.m
@@ -0,0 +1,31 @@
+//
+//  StratumInfoNotesViewController.m
+//  Strata Recorder
+//
+//  Created by daltman on 12/15/13.
+//  Copyright (c) 2013 Don Altman. All rights reserved.
+//
+
+#import "StratumInfoNotesViewController.h"
+
+@implementation StratumInfoNotesViewController
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
